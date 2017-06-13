88583fba5d5f8cf48f32928b574cc18d774d2921
puntnomads <puntnomads@gmail.com>
2017-06-13 17:16:21 +0400

    document ready function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
+$(document).ready(function(){
 window.onkeyup = keyup;
 var inputSearchTerm;
 function keyup(e) {
-inputTextValue = e.target.value; 
+inputTextValue = e.target.value;
   if (e.keyCode == 13) {
-var url ="https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=10&search="+inputTextValue+"&callback=?"; 
+var url ="https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=10&search="+inputTextValue+"&callback=?";
 for (var i = 0; i < 10; i++) {
-    (function(i) { 
+    (function(i) {
       $.getJSON(url, function (data) {
 $("#text_"+i).text(data[1][i]); $("#description_"+i).text(data[2][i]);
 $("#url_"+i).attr("href", data[3][i]);
       });
     })(i);
-}      
+}
   }
 }
+});
