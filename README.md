ead58ec15488a40e35facdd7ce7cf968de5e8f57
puntnomads <puntnomads@gmail.com>
2017-06-13 17:08:26 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Users can search for any term and the top 10 results from Wikipedia are displayed. The display consists of a title, a short description and a link to the main Wikipedia page. Also a random Wikipedia is opened if a button is clicked.
+
+Developed using JavaScript/jQuery and the Wikipedia Mediawiki API.
+ 
---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onkeyup = keyup;
+var inputSearchTerm;
+function keyup(e) {
+inputTextValue = e.target.value; 
+  if (e.keyCode == 13) {
+var url ="https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=10&search="+inputTextValue+"&callback=?"; 
+for (var i = 0; i < 10; i++) {
+    (function(i) { 
+      $.getJSON(url, function (data) {
+$("#text_"+i).text(data[1][i]); $("#description_"+i).text(data[2][i]);
+$("#url_"+i).attr("href", data[3][i]);
+      });
+    })(i);
+}      
+  }
+}
